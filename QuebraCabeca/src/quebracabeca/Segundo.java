d55a9eaab41719d6e2eed10b5858e1ba0d01a538
Fillipe Andrade Barreto <fillipe.barreto@faculdadesjorgeamado.com.br>
2025-06-13 19:14:29 -0300

    Segundo foi comentado completamente e o quebracabeca foi completado até sua metade

--- QuebraCabeca/src/quebracabeca/Segundo.java
+++ QuebraCabeca/src/quebracabeca/Segundo.java
@@ -2,26 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package quebracabeca;
+package quebracabeca;  //declaração da pasta ou pacote quebracabeca
 
-/**
- *
- * @author fillipe.barreto
- */
-public class Segundo implements Runnable{
+public class Segundo implements Runnable{ //criação da classe pública segundo que implementa o runnable para contar os segundos
     
     
     
     @Override
-    public void run(){
+    public void run(){ //sobrescreve o método run presente em todas as classes do java para executar da forma abaixo
         
-    int segundos = Integer.parseInt(quebracabecaJFRAME.tempo.getText()); 
-    for(;;){ //laço infinito
-        quebracabecaJFRAME.tempo.setText(segundos+" Seg.");
-        segundos++;
-        try{
-            Thread.sleep(1000);
-        }catch (Exception e){
+    int segundos = Integer.parseInt(quebracabecaJFRAME.tempo.getText()); /*int segundo é inicializado com o valor presente na variável estatica tempo que uma label
+    presente no quebracacaJFRAME*/
+    for(;;){ //declaração de um laço infinito
+        quebracabecaJFRAME.tempo.setText(segundos+" Seg."); //impressão do valor dos segundos na jlabel tempo
+        segundos++; //acreção do valor da variável segundos
+        try{ //tenta realizar a seguinte ação. 
+            Thread.sleep(1000); // espera 1 segundo para realizar mais uma repetição do laço
+        }catch (Exception e){ // caso o try não funcione ele realiza a segunite ação:
             
             
         }
